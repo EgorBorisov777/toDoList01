84c73306e782ac1d8b26ac8f397c67bfcf457dd6
Egor <102162946+EgorBorisov777@users.noreply.github.com>
2023-01-30 13:44:50 +0500

    Add files via upload

---
+++ components/AddTodo/AddTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { v1 as uuidv1 } from 'uuid'
+import { Row, Col, Button, FormControl } from 'react-bootstrap'
+import s from './AddTodo.module.css'
+
+function AddTodo({ todo, setTodo }) {
+  const [value, setValue] = useState('')
+
+  function saveTodo() {
+    setTodo([
+      ...todo,
+      {
+        id: uuidv1(),
+        title: value,
+        status: true,
+      },
+    ])
+    setValue('')
+  }
+  return (
+    <Row>
+      <Col className={s.addTodoForm}>
+        <FormControl
+          placeholder="Введите задачу"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={saveTodo} className={s.btn}>
+          Сохранить
+        </Button>
+      </Col>
+    </Row>
+  )
+}
+
+export default AddTodo
